8924d256b5c088194c109db2914dcc22e26157c1
AdithiBhatta <128722209+AdithiBhatta@users.noreply.github.com>
2023-05-28 23:30:53 +0530

    Add files via upload

---
+++ ASM.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ns
+module ASM( clk,stop,A, B, AQ);
+parameter N=8;
+integer i;
+input clk,stop;
+  input [N-1:0] A;
+  input [N-1:0] B;
+  
+  output reg [2*N-1:0]AQ;
+  reg [2*N-1:0]A1;
+  reg [N-1:0]B1;
+always@(posedge clk or posedge rst )
+begin
+  if (stop==1)
+		begin
+		AQ=0;
+		end
+		else 
+		begin
+		AQ=0;
+          A1[N-1:0]=A;
+          A1[2*N-1:N]=0;
+		B1=B;
+          for (i=0;i<N;i=i+1)
+        begin
+			if(B1[i]==1'b0)
+			begin
+				AQ=AQ+0;
+			end
+			else if (B1[i]==1'b1)
+			begin
+				AQ=AQ+(A1<<i);
+			end
+		end
+		end
+end
+endmodule
---
+++ ASM_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ns
+module ASM_tb;
+parameter N = 8;
+reg clk, stop;
+  reg [N-1:0] A;
+  reg [N-1:0] B;
+  wire [2*N-1:0] AQ;
+
+  ASM dut(clk, rst, A, B, AQ);
+
+initial 
+begin
+    clk = 0;
+    forever #10 clk = !clk;
+end
+initial
+begin
+    
+    stop = 1;
+    #20;
+    stop = 0;
+    A = 8'b00001001;
+    B = 8'b00000010;
+    #20;
+    stop = 1;
+    #20;
+    
+endmodule
